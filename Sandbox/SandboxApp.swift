bd82b6b18e299956afebbe028ebb8bf294e984fa
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2021-08-02 08:44:49 -0500

    Initial Commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Jeremy Fleshman on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
